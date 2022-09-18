ba40cb335abdedc3cd63f7fe21f5fe14ab9e8447
felleholmberg@gmail.com <felleholmberg@gmail.com>
2022-09-18 21:46:10 +0000

    developed reporting models

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,9 +1,9 @@
 select 
     ord.orderid, ord.orderdate, ord.shipdate, 
     ord.shipmode, (ord.ordersellingprice - ord.ordercostprice) as orderprofit,
-    ord.ordercostprice, ord.ordersellingprice,
-    cust.customername, cust.segment, cust.country,
-    prod.category, prod.productname, prod.subcategory 
+    ord.ordercostprice, ord.ordersellingprice, cust.customerid,
+    cust.customername, cust.segment, cust.country, 
+    prod.productid, prod.category, prod.productname, prod.subcategory 
 from {{ ref('raw_orders') }} as ord
 left join {{ ref('raw_customers') }} as cust
     on ord.customerid = cust.customerid
